edc12c2e474254ace1814d0dd1970d8d3c953943
Luis Carvalho <jrluiscarvalho@gmail.com>
2016-05-15 18:28:44 -0300

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name studyYeomanAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the studyYeomanAngularApp
+ */
+angular.module('studyYeomanAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name studyYeomanAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the studyYeomanAngularApp
+ */
+angular.module('studyYeomanAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
